0adaef051d725a81cb71fe7798a47582296f18f5
BurmitskijE <eric.burmitskij@aldi-nord.de>
2023-02-02 11:17:38 +0100

    commit

--- src/main/java/org/example/day2.java
+++ src/main/java/org/example/day2.java
@@ -36,6 +36,41 @@ public class day2 {
         }
         return score;
     }
+
+    /* Teil 2:
+    private static int getRoundScore(char opponent, char choice) {
+        int score = 0;
+        if (opponent == 'A' && choice == 'X') {
+        score = 3;
+        }
+        else if (opponent == 'A' && choice == 'Y') {
+            score = 1+3;
+        }
+        else if (opponent == 'A' && choice == 'Z') {
+            score = 2+6;
+        }
+        else if (opponent == 'B' && choice == 'X'){
+            score = 1;
+        }
+        else if (opponent == 'B' && choice == 'Y'){
+            score = 2+3;
+        }
+        else if (opponent == 'B' && choice == 'Z'){
+            score = 3+6;
+        }
+        else if (opponent == 'C' && choice == 'X' ){
+            score = 2;
+        }
+        else if (opponent == 'C' && choice == 'Y' ){
+            score = 3+3;
+        }
+        else if (opponent == 'C' && choice == 'Z' ){
+            score = 1+6;
+        }
+        return score;
+    }
+     */
+
     public static void main(String[] args) {
         int totalScore = 0;
         try {
