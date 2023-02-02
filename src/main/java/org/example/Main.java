e9df64f420c5b32a20f85a0593ca10f1af15d6f4
BurmitskijE <eric.burmitskij@aldi-nord.de>
2023-02-02 09:36:50 +0100

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,5 @@
 package org.example;
+
 import java.io.*;
 import java.util.*;
 
@@ -8,33 +9,55 @@ class Elf {
     public Elf(int calories) {
         this.calories = calories;
     }
+
+    public int getCalories() {
+        return this.calories;
+    }
 }
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        List<Elf> elves = new ArrayList<>();
-        int sumCalories = 0;
-        try (BufferedReader reader = new BufferedReader(new FileReader("input.txt"))) {
+    public static ArrayList<Elf> arrayifyCalories(String inputFile) {
+        ArrayList<Elf> array = new ArrayList<>();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(inputFile));
             String line;
+            int sumCalories = 0;
+
             while ((line = reader.readLine()) != null) {
                 if (line.isEmpty()) {
-                    elves.add(new Elf(sumCalories));
+                    array.add(new Elf(sumCalories));
                     sumCalories = 0;
                 } else {
-                    sumCalories += Integer.parseInt(line.trim());
+                    sumCalories += Integer.parseInt(line.strip());
                 }
             }
-            elves.add(new Elf(sumCalories));
+            array.add(new Elf(sumCalories));
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return array;
+    }
 
-        int maxCalories = Integer.MIN_VALUE;
-        int maxElfIndex = -1;
-        for (int i = 0; i < elves.size(); i++) {
-            if (elves.get(i).calories > maxCalories) {
-                maxCalories = elves.get(i).calories;
-                maxElfIndex = i;
-            }
+    public static int sumOfHighestThree(ArrayList<Elf> array) {
+        array.sort((a, b) -> Integer.compare(b.getCalories(), a.getCalories()));
+        int sumOfCalories = 0;
+
+        for (int i = 0; i < 3; i++) {
+            sumOfCalories += array.get(i).getCalories();
         }
-        System.out.println("Elf " + (maxElfIndex + 1) + " is carrying the most calories: " + maxCalories);
+        return sumOfCalories;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Elf> calorieList = arrayifyCalories("input.txt");
+        Elf maxElf = calorieList.stream().max((a, b) -> Integer.compare(a.getCalories(), b.getCalories())).get();
+        int index = calorieList.indexOf(maxElf);
+        System.out.println("Elf no. " + (index + 1) + " is the Elf with the most calories. He carries " + maxElf.getCalories() + " calories");
+        //Teil 2
+
+        int sum = sumOfHighestThree(calorieList);
+        System.out.println("The three richest Elves are carrying a total of " + sum + " calories");
     }
 }
